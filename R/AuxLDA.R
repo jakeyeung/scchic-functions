30ff1a09924d56cd6472fe579472d0376aee1da3
Jake Yeung <jakeyeung@gmail.com>
2019-08-07 10:08:08 +0200

    Order topics by entropy

--- R/AuxLDA.R
+++ R/AuxLDA.R
@@ -1,8 +1,23 @@
+OrderTopicsByEntropy <- function(tm.result, jquantile = 0.99){
+  # analyze topic matrix across cells
+  topics.long <- data.frame(cell = rownames(tm.result$topics), as.data.frame(tm.result$topics)) %>%
+    gather(key = "topic", value = "weight", -cell) %>%
+    rowwise() %>%
+    group_by(topic) %>%
+    mutate(zscore = scale(weight, center = TRUE, scale = TRUE))
+  topics.sum <- topics.long %>%
+    group_by(topic) %>% # do entropy on 1 to 99% of cells
+    filter(zscore < quantile(zscore, jquantile)) %>%
+    mutate(zscore.prob = exp(zscore) / sum(exp(zscore))) %>%
+    summarise(entropy = -sum(zscore.prob * log(zscore.prob))) %>%
+    arrange(entropy)
+  return(topics.sum)
+}
 
 DoLouvain <- function(topics.mat, custom.settings.louv, dat.umap.long = NULL){
   # Do Louvain for clustering
   dat.umap.louv <- umap(topics.mat, config = custom.settings.louv)
-  dat.umap.louv.long <- data.frame(umap1 = dat.umap.louv$layout[, 1], umap2 = dat.umap.louv$layout[, 2], cell = rownames(dat.umap.louv$layout), 
+  dat.umap.louv.long <- data.frame(umap1 = dat.umap.louv$layout[, 1], umap2 = dat.umap.louv$layout[, 2], cell = rownames(dat.umap.louv$layout),
                                    stringsAsFactors = FALSE)
   cell.indx <- hash(rownames(dat.umap.louv$knn$indexes), dat.umap.louv$knn$indexes[, 1])
   cell.indx.rev <- hash(dat.umap.louv$knn$indexes[, 1], rownames(dat.umap.louv$knn$indexes))
@@ -44,9 +59,9 @@ SelectBestPeak <- function(jpeaks, regions.annot = NULL, tm.result){
   terms.sub <- tm.result$terms[, jpeaks]
   jmax <- apply(terms.sub, 2, max)
   jmax.i <- apply(terms.sub, 2, which.max)  # which topic max occurs. Often they agree.
-  
+
   print(unique(jmax.i))
-  
+
   jpeak <- jpeaks[which(jmax == max(jmax))]
   return(jpeak)
 }
@@ -80,36 +95,36 @@ LoadLDABins <- function(jmark, jbin=TRUE, top.thres=0.995, inf = NULL, convert.c
     out.lda <- out.lda[[i]]
   }
 
-  
+
   if (add.chr.prefix){
     out.lda@terms <- paste0("chr", out.lda@terms)
   }
-  
+
   kchoose <- out.lda@k
   tm.result <- posterior(out.lda)
-  
+
   if (convert.chr20.21.to.X.Y){
     colnames(tm.result$terms) <- gsub("chr20", "chrX", colnames(tm.result$terms))
     colnames(tm.result$terms) <- gsub("chr21", "chrY", colnames(tm.result$terms))
   }
-  
+
   regions <- data.frame(seqnames = sapply(colnames(tm.result$terms), GetChromo),
                         start = sapply(colnames(tm.result$terms), GetStart),
-                        end = sapply(colnames(tm.result$terms), GetEnd), 
+                        end = sapply(colnames(tm.result$terms), GetEnd),
                         stringsAsFactors = FALSE)
   rownames(regions) <- colnames(tm.result$terms)
   regions <- subset(regions, !seqnames %in% c("chr20", "chr21"))
-  
+
   regions.range <- makeGRangesFromDataFrame(as.data.frame(regions))
-  regions.annotated <- as.data.frame(annotatePeak(regions.range, 
-                                                  TxDb=TxDb.Mmusculus.UCSC.mm10.knownGene, 
+  regions.annotated <- as.data.frame(annotatePeak(regions.range,
+                                                  TxDb=TxDb.Mmusculus.UCSC.mm10.knownGene,
                                                   annoDb='org.Mm.eg.db'))
   regions.annotated$region_coord <- names(regions.range)
-  
+
   topic.regions <- lapply(seq(kchoose), function(clst){
     return(SelectTopRegions(tm.result$terms[clst, ], colnames(tm.result$terms), method = "thres", method.val = top.thres))
   })
-  
+
   return(list('out.lda' = out.lda, 'tm.result' = tm.result, 'topic.regions' = topic.regions, 'regions.annotated' = regions.annotated, 'count.mat' = count.mat))
 }
 
@@ -189,16 +204,16 @@ SelectTopRegions <- function(beta.row, regions, method = "thres", method.val = 0
 
 GetCountMatFromLDA <- function(out.lda){
   # https://stackoverflow.com/questions/20004493/convert-simple-triplet-matrixslam-to-sparse-matrixmatrix-in-r
-  count.mat <- Matrix::sparseMatrix(i=out.lda@wordassignments$i, 
-                                  j=out.lda@wordassignments$j, 
-                                  x=out.lda@wordassignments$v, 
+  count.mat <- Matrix::sparseMatrix(i=out.lda@wordassignments$i,
+                                  j=out.lda@wordassignments$j,
+                                  x=out.lda@wordassignments$v,
                                   dims=c(out.lda@wordassignments$nrow, out.lda@wordassignments$ncol))
   return(count.mat)
 }
 
 
 .modelMatSelection <- function(
-  # from cisTopics package 
+  # from cisTopics package
   object,
   target,
   method,
@@ -208,12 +223,12 @@ GetCountMatFromLDA <- function(out.lda){
   if (length(object@selected.model) < 1){
     stop('Please, run selectModel() first.')
   }
-  
+
   if (target == 'cell'){
     if (method == 'Z-score'){
       modelMat <- scale(object@selected.model$document_expects, center=TRUE, scale=TRUE)
     }
-    
+
     else if (method == 'Probability'){
       alpha <- object@calc.params[['runModels']]$alpha/length(object@selected.model$topic_sums)
       modelMat <- apply(object@selected.model$document_sums, 2, function(x) {(x + alpha)/sum(x + alpha)})
@@ -224,7 +239,7 @@ GetCountMatFromLDA <- function(out.lda){
     colnames(modelMat) <- object@cell.names
     rownames(modelMat) <- paste0('Topic', 1:nrow(modelMat))
   }
-  
+
   else if (target == 'region'){
     if (!all.regions){
       if (length(object@binarized.cisTopics) < 1){
@@ -234,39 +249,39 @@ GetCountMatFromLDA <- function(out.lda){
         regions <- unique(unlist(lapply(object@binarized.cisTopics, rownames)))
       }
     }
-    
+
     topic.mat <- object@selected.model$topics
-    
+
     if (method == 'NormTop'){
       normalizedTopics <- topic.mat/(rowSums(topic.mat) + 1e-05)
       modelMat <- apply(normalizedTopics, 2, function(x) x * (log(x + 1e-05) - sum(log(x + 1e-05))/length(x)))
     }
-    
+
     else if (method == 'Z-score'){
       modelMat <- scale(object@selected.model$topics, center=TRUE, scale=TRUE)
     }
-    
+
     else if (method == 'Probability'){
       beta <- object@calc.params[['runModels']]$beta
       topic.mat <- object@selected.model$topics
       modelMat <-  (topic.mat + beta)/rowSums(topic.mat + beta)
     }
-    
+
     else{
       stop('Incorrect method selected. Chose "NormTop", "Z-score" and "Probability".')
     }
-    
+
     colnames(modelMat) <- object@region.names
     rownames(modelMat) <- paste0('Topic', 1:nrow(modelMat))
-    
+
     if (!all.regions){
       modelMat <- modelMat[,regions]
     }
   }
-  
+
   else{
     stop('Please, provide target="cell" or "region".')
   }
-  
+
   return(modelMat)
 }
