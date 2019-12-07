968a0b1d0c20e0503e7792e9ce19e3643f5ed632
Jake Yeung <jakeyeung@gmail.com>
2019-12-07 21:07:41 +0000

    Add new function to removing blacklist

---
+++ R/BedFunctions.R
@@ -0,0 +1,22 @@
+# Jake Yeung
+# Date of Creation: 2019-12-07
+# File: ~/projects/scchicFuncs/R/BedFunctions.R
+#
+
+FilterBinsByBlacklist <- function(rnames.all.common, blfile){
+  assertthat::assert_that(file.exists(bfile))
+  rnames.gr <- makeGRangesFromDataFrame(data.frame(seqnames = sapply(rnames.all.common, GetChromo, add.chr=FALSE),
+                                                   start = sapply(rnames.all.common, GetStart),
+                                                   end = sapply(rnames.all.common, GetEnd)))
+  bl.gr <- LoadBlacklist(inf = blfile)
+  overlaps <- findOverlaps(bl.gr, rnames.gr)
+  indx <- seq(length(rnames.gr))
+  bl.hits.i <- unique(subjectHits(overlaps))
+  bl.hits.l <- !indx %in% bl.hits.i
+  rnames.gr.filt <- rnames.gr[bl.hits.l]
+  rnames.gr.badbins <- rnames.gr[!bl.hits.l]
+  rnames.all.common.blfilt <- names(rnames.gr.filt)
+  return(rnames.all.common.blfilt)
+}
+
+
