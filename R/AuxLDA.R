b01348897619ce2621250a1452e90917868c2457
Jake Yeung <j.yeung@hubrecht.eu>
2020-05-17 18:10:49 +0200

    Do things gene wise

--- R/AuxLDA.R
+++ R/AuxLDA.R
@@ -9,6 +9,138 @@ AddTopicToTmResult <- function(tm.result, jsep = ""){
   return(tm.result)
 }
 
+AnnotateCoordsFromList.GeneWise <- function(coords.vec,
+                                            inf.tss="/Users/yeung/data/scchic/tables/gene_tss_winsize.50000.bed",
+                                            txdb = TxDb.Mmusculus.UCSC.mm10.knownGene,
+                                            annodb = "org.Mm.eg.db",
+                                            chromos.keep=c(paste("chr", seq(19), sep = ""), "chrX", "chrY")){
+  library(ChIPseeker)
+  assertthat::assert_that(file.exists(inf.tss))
+  regions <- data.frame(seqnames = sapply(coords.vec, GetChromo),
+                        start = sapply(coords.vec, GetStart),
+                        end = sapply(coords.vec, GetEnd),
+                        stringsAsFactors = FALSE)
+  rownames(regions) <- coords.vec
+  # regions <- subset(regions, !seqnames %in% c("chr20", "chr21"))
+  print("Chromos to keep")
+  print(chromos.keep)
+  regions <- subset(regions, seqnames %in% chromos.keep)
+  
+  regions.range <- makeGRangesFromDataFrame(as.data.frame(regions))
+  regions.annotated <- as.data.frame(annotatePeak(regions.range,
+                                                  # TxDb=TxDb.Mmusculus.UCSC.mm10.knownGene,
+                                                  TxDb=txdb,
+                                                  # annoDb='org.Mm.eg.db'))
+                                                  annoDb=annodb))
+  regions.annotated$region_coord <- names(regions.range)
+  
+  tss.dat <- fread(inf.tss, col.names = c("seqnames", "start", "end", "tssname"))
+  tss.dat$gene <- sapply(tss.dat$tssname, function(x) strsplit(x, ";")[[1]][[2]])
+  
+  annots.biomart <- regions.annotated %>%
+    mutate(midpt = start + (end - start) / 2)
+  
+  annots.gr <- makeGRangesFromDataFrame(annots.biomart %>% dplyr::select(seqnames, start, end, SYMBOL, region_coord), keep.extra.columns = TRUE)
+  annots.tss.gr <- makeGRangesFromDataFrame(tss.dat, keep.extra.columns = TRUE)
+  
+  out2 <- findOverlaps(annots.tss.gr, annots.gr, type = "any")
+  # out2 <- findOverlaps(annots.gr, annots.tss.gr, type = "any")
+  
+  out2.df = data.frame(annots.gr[subjectHits(out2),], annots.tss.gr[queryHits(out2),]) %>%
+    mutate(midpt = start + round(width / 2),
+           midpt.1 = start.1 + round(width.1 / 2),
+           dist.to.tss = midpt.1 - midpt)
+  
+  # # filter closest
+  # out2.df.closest <- out2.df %>%
+  #   group_by(region_coord) %>%
+  #   filter(abs(dist.to.tss) == min(abs(dist.to.tss)))
+  
+  # terms.new <- paste(out2.df.closest$region_coord, out2.df.closest$gene, sep = ";")
+  # terms.hash <- hash::hash(out2.df.closest$region_coord, terms.new)
+  
+  return(list('regions.annotated' = regions.annotated, 'out2.df' = out2.df, 'out2' = out2, "annots.tss.gr" = annots.tss.gr, 'annots.gr' = annots.gr))
+}
+
+
+AnnotateBins2 <- function(terms.mat, top.thres=0.995, inf.tss="/Users/yeung/data/scchic/tables/gene_tss_winsize.50000.bed", txdb = TxDb.Mmusculus.UCSC.mm10.knownGene, annodb = "org.Mm.eg.db", chromos.keep=c(paste("chr", seq(19), sep = ""), "chrX", "chrY"), skip.split = FALSE){
+  assertthat::assert_that(file.exists(inf.tss))
+  assertthat::assert_that(class(terms.mat) == "matrix")
+  regions <- data.frame(seqnames = sapply(colnames(terms.mat), GetChromo),
+                        start = sapply(colnames(terms.mat), GetStart),
+                        end = sapply(colnames(terms.mat), GetEnd),
+                        stringsAsFactors = FALSE)
+  rownames(regions) <- colnames(terms.mat)
+  # regions <- subset(regions, !seqnames %in% c("chr20", "chr21"))
+  print("Chromos to keep")
+  print(chromos.keep)
+  regions <- subset(regions, seqnames %in% chromos.keep)
+  
+  regions.range <- makeGRangesFromDataFrame(as.data.frame(regions))
+  regions.annotated <- as.data.frame(annotatePeak(regions.range,
+                                                  # TxDb=TxDb.Mmusculus.UCSC.mm10.knownGene,
+                                                  TxDb=txdb,
+                                                  # annoDb='org.Mm.eg.db'))
+                                                  annoDb=annodb))
+  regions.annotated$region_coord <- names(regions.range)
+  
+  topic.regions <- lapply(seq(nrow(terms.mat)), function(clst){
+    return(SelectTopRegions(terms.mat[clst, ], colnames(terms.mat), method = "thres", method.val = top.thres))
+  })
+  
+  print(paste("Using TSS definitions from:", inf.tss))
+  terms.long <- data.frame(term = colnames(terms.mat), as.data.frame(t(terms.mat)), stringsAsFactors = FALSE) %>%
+    gather(key = "topic", value = "weight", -term) %>%
+    mutate(topic = gsub("X", "", topic)) %>%
+    group_by(topic) %>%
+    arrange(desc(weight)) %>%
+    mutate(rnk = seq(length(weight))) %>%
+    rowwise()
+  terms.filt.top <- terms.long %>%
+    # filter(rnk < 1000) %>%  # DO GENOME WIDE
+    rowwise()
+  tss.dat <- fread(inf.tss, col.names = c("seqnames", "start", "end", "tssname"))
+  tss.dat$gene <- sapply(tss.dat$tssname, function(x) strsplit(x, ";")[[1]][[2]])
+  
+  annots.biomart <- regions.annotated %>%
+    mutate(midpt = start + (end - start) / 2) %>%
+    filter(region_coord %in% terms.filt.top$term)
+  
+  annots.gr <- makeGRangesFromDataFrame(annots.biomart %>% dplyr::select(seqnames, start, end, SYMBOL, region_coord, ENSEMBL), keep.extra.columns = TRUE)
+  annots.tss.gr <- makeGRangesFromDataFrame(tss.dat, keep.extra.columns = TRUE)
+  
+  out <- findOverlaps(annots.tss.gr, annots.gr, type = "within")
+  out2 <- findOverlaps(annots.gr, annots.tss.gr, type = "any")
+  
+  out2.df = data.frame(annots.gr[queryHits(out2),], annots.tss.gr[subjectHits(out2),]) %>%
+    mutate(midpt = start + round(width / 2),
+           midpt.1 = start.1 + round(width.1 / 2),
+           dist.to.tss = midpt.1 - midpt)
+  
+  # filter closest
+  out2.df.closest <- out2.df %>%
+    group_by(region_coord) %>%
+    filter(abs(dist.to.tss) == min(abs(dist.to.tss)))
+  
+  terms.new <- paste(out2.df.closest$region_coord, out2.df.closest$gene, sep = ";")
+  terms.hash <- hash::hash(out2.df.closest$region_coord, terms.new)
+  
+  terms.annot <- terms.filt.top %>%
+    mutate(termgene = ifelse(!is.null(terms.hash[[term]]), terms.hash[[term]], NA))
+  
+  terms.filt <- terms.filt.top %>%
+    mutate(termgene = ifelse(!is.null(terms.hash[[term]]), terms.hash[[term]], NA)) %>%
+    filter(!is.na(termgene))
+  if (!skip.split){
+    terms.filt <- terms.filt %>%
+      mutate(gene = sapply(termgene, function(x) strsplit(x, ";")[[1]][[2]])) %>%
+      group_by(gene)  # dont filter use
+  }
+  return(list('topic.regions' = topic.regions, 'regions.annotated' = regions.annotated, 'terms.annot' = terms.annot, 'out2.df.closest' = out2.df.closest, 'terms.filt' = terms.filt))
+}
+
+
+
 AnnotateCoordsFromList <- function(coords.vec,
                                    inf.tss="/Users/yeung/data/scchic/tables/gene_tss_winsize.50000.bed",
                                    txdb = TxDb.Mmusculus.UCSC.mm10.knownGene,
