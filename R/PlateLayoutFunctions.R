66845c4eb46713517783d5b4b1e056c29799e74f
Jake Yeung <j.yeung@hubrecht.eu>
2020-11-12 12:56:57 +0100

    Add functions for AllMerged DE analysis

--- R/PlateLayoutFunctions.R
+++ R/PlateLayoutFunctions.R
@@ -3,6 +3,52 @@
 # File: ~/projects/scchic-functions/R/PlateLayoutFunctions.R
 # 
 
+
+
+IsRound1 <- function(samp, mark = "H3K4me1"){
+  if (mark == "H3K4me1"){
+    wt.plates <- c("B6-13W1-BM-H3K4me1", "PZ-ChIC-Bl6-BM-H3K4me1-Index")
+    linneg.plates <- c("PZ-ChIC-Bl6-BM-lin-H3K4me1-")
+    hsc.plates <- c("-stem-cells-")
+  } else if (mark == "H3K4me3"){
+    wt.plates <- c("B6-13W1-BM-H3K4me3")
+    linneg.plates <- c("-Linneg-")
+    hsc.plates <- c("-B6BMSC-")
+  } else if (mark == "H3K27me3") {
+    wt.plates <- c("B6-13W1-BM-")
+    linneg.plates <- c("-Linneg-")
+    hsc.plates <- c("-B6BMSC-")
+  } else if (mark == "H3K9me3") {
+    wt.plates <- c("B6-13W1-BM-")
+    linneg.plates <- c("-Linneg-")
+    hsc.plates <- c("BMSC-")
+  } else {
+    print(paste(mark, "not yet coded"))
+  }
+  wt.plates.grep <- paste(wt.plates, collapse = "|")
+  linneg.plates.grep <- paste(linneg.plates, collapse = "|")
+  hsc.plates.grep <- paste(hsc.plates, collapse = "|")
+  is.wt <- grepl(wt.plates.grep, samp)
+  is.linneg <- grepl(linneg.plates.grep, samp)
+  is.hsc <- grepl(hsc.plates.grep, samp)
+  bool.vec <- c(is.wt, is.linneg, is.hsc)
+  # print(bool.vec)
+  if (all(!bool.vec)){
+    return("Round2")
+  }
+  indx <- which.max(bool.vec)
+  if (indx == 1){
+    cond <- "Unenriched"
+  } else if (indx == 2){
+    cond <- "Linneg"
+  } else if (indx == 3){
+    cond <- "StemCell"
+  } else {
+    print(paste("Unexpected indx should be 1, 2, 3:", cond))
+  }
+}
+
+
 SplitGetLast <- function(x, jsplit = "-"){
   # get last element after splitting
   xsplit <- strsplit(x, jsplit)[[1]]
